5377634a1a30f0d38233143232702cebde0a7efb
termi <termi_uc@inbox.ru>
2013-08-08 22:52:26 +0400

    More esprima unsupported syntax

--- other/not-supported-by-esprima.js
+++ other/not-supported-by-esprima.js
@@ -1,7 +1,10 @@
-// Esprima parser bug list
+// Esprima parser bugs list
 
 //{a} is ObjectExpression insteadof ObjectPattern
 try {} catch({a}){  }
 
-//
+// Error: Line 1: Unexpected token )
 console.log( ( (a)=>a )(1) )
+
+// Error: Line 1: Spread must be the final element of an element list
+var arr = [...[1], 2]
