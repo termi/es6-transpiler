701bfd05662cd5283902c7a65d57d4d677aa8d0d
termi <termi_uc@inbox.ru>
2013-10-22 21:29:10 +0400

    todo.js update

--- other/todo.js
+++ other/todo.js
@@ -1,3 +1,14 @@
+// Forbidden destructuring
+{
+      var test51 = [3, 2, 1];
+      var [...[...test52]] = [test51.reverse()];
+      test51.push(4);
+      console.log(test51.join("|") === "1|2|3|4", test52.join("|") === "1|2|3");
+}
+
+// ---------------------------------------------------
+
+// Esprima support
 let obj = {
 	test: (function({a = 1} = {}){
 		let d = 123, res;
@@ -10,6 +21,9 @@ let obj = {
 	})()
 }
 
+// ---------------------------------------------------
+
+// Array comprehensions
 [for (x of ['a', 'b', 'c']) for (y of [1, 2, 3]) for (z of [9, 8, 7]) (x+y+z)];// ->
 //function ITER$0(v,f,fn){if(v){var ia=Array.isArray(v);if(ia&&!fn)return f?v.slice():v;if(ia||(typeof v==='object'&&typeof v['iterator']==='function'))return Array['from'](v,fn);}throw new Error(v+' is not iterable')};var x, y, z;
 //[].concat.apply([], ITER$0(['a', 'b', 'c'], false, function(x){return [].concat.apply([], ITER$0([1, 2, 3], false, function(y){return ITER$0([9, 8, 7], false, function(z){return (x+y+z)})}))}));
@@ -30,6 +44,7 @@ forOf([], arr, function(x){  forOf(this, arr2, function(){ if(x % 2)this.push(x 
 
 // --------------------------------------------------------------------
 
+// Destructuring refactoring
 function test1({a}) {
 	function test2() {
 		console.log(test1.arguments, a)
