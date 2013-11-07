269e16036c75d397f56c1aa7efea1a6227acc0a8
termi <termi_uc@inbox.ru>
2013-11-07 19:05:40 +0400

    tiny tests update

--- tests/es6-spread-out.js
+++ tests/es6-spread-out.js
@@ -4,7 +4,7 @@ function ITER$0(v,f){if(v){if(Array.isArray(v))return f?v.slice():v;if(typeof v=
 	var i = 0;
 	var a = [];
 	var c = ["c"];
-	var b = [-2, -1, "|"].concat(ITER$0((a.push(i++),a), true), "idx", [i, "-"], ITER$0((a.push(i++),a), true), "idx", [i, "-"], ITER$0((a.push(i++),a), true), "idx", [i, "-"], ITER$0(c))
+	var b = [-2, -1, "|"].concat(ITER$0((a.push(i++),a), true), "idx", [i, "-"], ITER$0((a.push(i++),a), true), "idx", [i, "-"], ITER$0((a.push(i++),a), true), "idx", [i, "-"], ITER$0(c));
 
 	console.log( b.join("|") === [-2, -1, "|", 0, "idx", 1, "-", 0, 1, "idx", 2, "-", 0, 1, 2, "idx", 3, "-", "c"].join("|") )
 }
@@ -44,7 +44,7 @@ test.apply(null, [1].concat([].concat([2], [[3]])))
 
 	var b$2 = (function(){var SLICE$0 = Array.prototype.slice;var a = arguments[0];if(a === void 0)a = 9;var b = arguments[1];if(b === void 0)b = 8;var c = arguments[2];if(c === void 0)c = 7;var rest = SLICE$0.call(arguments, 3);return [a, b, c].concat(ITER$0(rest))
 
-	}).apply(null, ITER$0(a$3))
+}).apply(null, ITER$0(a$3))
 
 	console.log(b$2.join("|") === [9, 8, 7, 6, 5, 4].join("|"))
 }
